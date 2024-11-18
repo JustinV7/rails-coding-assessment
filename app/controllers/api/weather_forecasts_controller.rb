51a800594e743b4cea72a33fb3f986fea8c577e5
Justin Van Dyke <justin.vandyke@gmail.com>
2024-11-17 17:59:14 -0700

    forecast response documentation

--- app/controllers/api/weather_forecasts_controller.rb
+++ app/controllers/api/weather_forecasts_controller.rb
@@ -5,8 +5,16 @@ module Api
     rescue_from ThirdParty::Errors::ServerError, with: :fatal_error
     rescue_from ThirdParty::Errors::UnexpectedResponseError, with: :fatal_error
 
+    def_param_group :forecast_data do
+      property :temp, Float, desc: 'Forecasted temperature at time of Forecast.'
+      property :min, Float, desc: 'Forecasted Low Temperature for the day.'
+      property :max, Float, desc: 'Forecasted High Temperature for the day.'
+      property :datetime, String, desc: 'DateTime string for forecast values.'
+    end
+
     api :POST, '/api/weather_forecasts/location'
     param :address, String, desc: 'Location address for weather API call.', required: true
+    returns array_of: :forecast_data
     def location
       address = params[:address]
       @subject = address
